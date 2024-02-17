147b339e01efd7888b9a76a68a5752571da81b86
Joan <58220974+joan-cb@users.noreply.github.com>
2024-02-17 13:07:13 +0000

    minor change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const postRoute = require("./routes/post");
 require("dotenv/config");
 
 
-console.log("app starting...");
+console.log("app is starting...");
 app.use(bodyParser.json());
 app.use("/userManagement", authRoute);
 app.use("/postManager", postRoute);
